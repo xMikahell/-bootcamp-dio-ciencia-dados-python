0d921c9c6d8bf6719536e5d0df8cb14baceef6d4
xMikahell <mikaelvictorf@protonmail.com>
2026-03-07 14:33:49 -0300

    adicionando aulas restantes

---
+++ PrimeirosPassos/Aula2.4.Convertendo_Tipos.py
@@ -0,0 +1,17 @@
+#Aula sobre converter tipos de variaveis
+#string para float ocasiona erro
+
+print(int(1.982838))
+print(int("10"))
+print(float("10.10"))
+print(float(100)) 
+
+
+#perguntar tipo e classe da variavel
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100 / 2) # uma barra vem flutuante
+print(100 // 2)# duas barras é a parte inteira do numero
---
+++ PrimeirosPassos/Aula2.5.Funcoes_entrada_saida.py
@@ -0,0 +1,25 @@
+#Aula sobre funções
+
+
+#exemplo
+
+nome = "Mikael"
+sobrenome = "Sales"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n") #termina nos tres pontos e quebra uma linha
+print(nome, sobrenome, sep="#") #adicionando um separador '#' 
+
+#---------------
+
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
+
+#----------(meu experimento)--------
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+
