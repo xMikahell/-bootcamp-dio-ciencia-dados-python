9564ddac9e93b66572efd5107eb8d306d5cdcb2c
xMikahell <mikaelvictorf@protonmail.com>
2026-03-09 14:10:52 -0300

    Aula sobre git e github, comandos etc

---
+++ 2.Curso_e_Ferramentas/Aula.1.CursoeFerramentas.py
@@ -0,0 +1,6 @@
+#Aulas teoricas
+#creio que sera só 1 arquivo de comentario
+#Explicando sobre Git e Githhub (Ferramentas)
+#baixar e intalar o git 
+#ensinando como configurar o github/repositorio via gitbash
+
