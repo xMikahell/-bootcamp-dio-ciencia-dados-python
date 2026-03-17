933bd0f3ff05f4291eb43fce6e2f273dc33fe22f
xMikahell <mikaelvictorf@protonmail.com>
2026-03-17 16:59:47 -0300

    aula 6

---
+++ 6.Listas e dados/6.1.Listas.py
@@ -0,0 +1,24 @@
+#Listas em python podem armazenar de maneira sequencial qualquer tipo \n
+#de objeto. Listas são objetos mutaveis, portanto podemos alterar seus valores apos a criação
+
+#exemplo:
+
+frutas = ["laranja", "maçã", "uva"]
+
+frutas = []
+
+letras = list ("python")
+
+numeros = list(range(10))
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+
+lista = ["p", "y", "t", "h", "o", "n"]
+
+lista [2:]
+
+lista [:2]
+lista [1:3]
+lista [0:3:2]
+lista [::]
+lista [::-1]
