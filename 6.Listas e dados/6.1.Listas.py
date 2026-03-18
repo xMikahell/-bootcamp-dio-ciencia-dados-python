9708ae4b80a90f3d226123437a64a0a407f323ae
xMikahell <mikaelvictorf@protonmail.com>
2026-03-18 18:49:40 -0300

    atualizando

--- 6.Listas e dados/6.1.Listas.py
+++ 6.Listas e dados/6.1.Listas.py
@@ -22,3 +22,27 @@ lista [1:3]
 lista [0:3:2]
 lista [::]
 lista [::-1]
+
+#filtro versao 1
+
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+
+#filtro versao 2 
+
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+
+#modificando valores versao 1
+
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = []
+
+for numero in numeros:
+    quadrado.append(numero ** 2)
+
